a37586ea92c7e80b8828cd844079f37c1c14ce00
jalagor <johnrossrogla@gmail.com>
2020-02-04 11:43:48 -0700

    skeleton built

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component } from 'react';
 import './App.css';
+import {PictureScroll} from './components/PictureScroll'
+import {NasaHeader} from './components/NasaHeader'
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    pictures: []
+  }
+  render(){
+
+    return (
+      <div className="App">
+       
+        <NasaHeader/>
+        <PictureScroll/>
+
+      
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NasaHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NasaHeader = () => {
+    return (
+        <div>
+        <h1>Nasa App</h1>
+        </div>
+    )
+}
